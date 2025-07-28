ea4d4d4a429bad2594da234b6e138e15bcfe4232
Thushana LS <dhushanals@example.com>
2025-07-28 15:43:00 +0530

    feat: add ThemeProvider and integrate it into RootLayout
    - Created `ThemeProvider` to handle theme mode (light/dark/system) using Zustand store
    - Wrap app content with `ThemeProvider` inside `ApiProvider` in `RootLayout`s

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ApiProvider } from "@/providers/ApiProvider";
+import ThemeProvider from "@/providers/ThemeProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +29,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ApiProvider> {children}</ApiProvider>
+        <ApiProvider>
+          <ThemeProvider>{children}</ThemeProvider>
+        </ApiProvider>
       </body>
     </html>
   );

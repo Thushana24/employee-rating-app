7af0ae9ce261b9ba4fbde63f28095a0bbca8f9e7
Thushana LS <dhushanals@example.com>
2025-07-28 14:43:56 +0530

    refactor(api): move assign-employee-to-supervisor route to organization path feat(schema): add CriteriaSchema for evaluation input validation

---
+++ src/schemas/criteria.schema.ts
@@ -0,0 +1,11 @@
+import { z } from "zod";
+const CriteriaSchema = z.object({
+  name: z
+    .string("Criteria name is required")
+    .min(2, { message: "should be atleast two characters long" }),
+  maxScore: z
+    .number()
+    .int()
+    .positive({ message: "maxScore must be a positive integer" }),
+});
+export { CriteriaSchema };

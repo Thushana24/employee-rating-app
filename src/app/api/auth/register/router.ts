9cd93c8cf83986cd44d006dd2a1232c86f142964
Thushana LS <dhushanals@example.com>
2025-07-28 12:39:47 +0530

    feat(auth): implement user registration with organization creation and token generation
    - Added `POST /auth/register` route to create user and associated organization
    - Ensures unique email and organization name with conflict checks
    - Hashes password securely using Argon2
    - Assigns OWNER role with predefined permissions
    - Returns JWT token with organization context upon successful registration

---
+++ src/app/api/auth/register/router.ts
@@ -0,0 +1,113 @@
+import prisma from "@/lib/prisma";
+import RegisterUserSchema from "@/schemas/user.schema";
+import { UserRole } from "@prisma/client";
+import argon2 from "argon2";
+import { NextRequest, NextResponse } from "next/server";
+import { OWNER_PERMISSIONS } from "../permissions";
+import generateToken, { IJWTPayload } from "../helpers/generateToken";
+import handleError from "../helpers/handleError";
+
+export async function POST(request: NextRequest) {
+  try {
+    const body = await request.json();
+    const validatedData = RegisterUserSchema.parse(body);
+
+    const isUserExist = await prisma.user.findUnique({
+      where: { email: validatedData.email },
+    });
+    if (isUserExist) {
+      return NextResponse.json(
+        {
+          success: false,
+          error: {
+            code: "USER_ALREADY_EXISTS",
+            message: "User is already exist",
+          },
+        },
+        { status: 409 },
+      );
+    }
+
+    const isOrganizationExist = await prisma.organization.findUnique({
+      where: { name: validatedData.organizationName },
+    });
+    if (isOrganizationExist) {
+      return NextResponse.json(
+        {
+          success: false,
+          error: {
+            code: "ORGANIZATION_NAME_ALREADY_EXISTS",
+            message: "Organization name is already exist",
+          },
+        },
+        { status: 409 },
+      );
+    }
+
+    const { firstName, lastName, email, password, organizationName } =
+      validatedData;
+
+    // Hashed the password securely
+    const hashedPassword = await argon2.hash(password);
+
+    //created a user using the validated data
+    const result = await prisma.$transaction(
+      async (tx) => {
+        const user = await tx.user.create({
+          data: {
+            firstName,
+            lastName,
+            email,
+            password: hashedPassword,
+          },
+        });
+
+        const organization = await tx.organization.create({
+          data: {
+            name: organizationName,
+            Owner: {
+              connect: {
+                id: user.id,
+              },
+            },
+          },
+        });
+
+        const organizationMember = await tx.organizationMember.create({
+          data: {
+            organizationId: organization.id,
+            userId: user.id,
+            role: UserRole.OWNER,
+            permissions: OWNER_PERMISSIONS,
+          },
+        });
+
+        return { user, organization, organizationMember };
+      },
+      { timeout: 15000 },
+    );
+
+    const token = generateToken<IJWTPayload>({
+      id: result.user.id,
+      organizations: [result.organizationMember],
+    });
+
+    const { password: _, ...userResponse } = result.user;
+    const { ...orgResponse } = result.organization;
+
+    return NextResponse.json(
+      {
+        success: true,
+        data: {
+          user: userResponse,
+          organization: orgResponse,
+        },
+        token,
+        message: "User created successfully.",
+      },
+      { status: 201 },
+    );
+  } catch (error) {
+    return handleError(error, "Failed to register user");
+  }
+}

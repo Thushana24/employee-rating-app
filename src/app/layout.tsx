174d05f62b3f57f61b2f3373e29d7cea69df743d
Thushana LS <dhushanals@example.com>
2025-07-28 15:49:10 +0530

    chore: add ReactQueryClientProvider to app layout
    - Created ReactQueryClientProvider component to wrap app with TanStack Query's QueryClientProvider
    - Integrated ReactQueryClientProvider in RootLayout to provide react-query context globally
    - Wrapped ThemeProvider inside ReactQueryClientProvider for consistent context usage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ApiProvider } from "@/providers/ApiProvider";
 import ThemeProvider from "@/providers/ThemeProvider";
+import { ReactQueryClientProvider } from "@/providers/ReactQueryClientProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,7 +31,9 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ApiProvider>
-          <ThemeProvider>{children}</ThemeProvider>
+          <ReactQueryClientProvider>
+            <ThemeProvider>{children}</ThemeProvider>
+          </ReactQueryClientProvider>
         </ApiProvider>
       </body>
     </html>

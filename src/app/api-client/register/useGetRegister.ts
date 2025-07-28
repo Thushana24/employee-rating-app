2ab08c59ac7e50b362a133679a86d4e4370d6423
Thushana LS <dhushanals@example.com>
2025-07-28 15:51:21 +0530

    feat: add useRegister hook for user registration mutation

---
+++ src/app/api-client/register/useGetRegister.ts
@@ -0,0 +1,26 @@
+import { useApi } from "@/providers/ApiProvider";
+import { useCreateMutation } from "../apiFactory";
+import { User } from "@prisma/client";
+import { RegisterInput } from "@/app/api/auth/types";
+
+export const useRegister = ({
+  invalidateQueryKey,
+}: {
+  invalidateQueryKey?: unknown[];
+}) => {
+  const { jsonApiClient } = useApi();
+
+  return useCreateMutation<
+    Record<string, any>,
+    RegisterInput,
+    { data: { user: User; token: string } },
+    { data: { user: User; token: string } }
+  >({
+    apiClient: jsonApiClient,
+    method: "post",
+    url: "/auth/register",
+    errorMessage: "Failed to register.",
+    invalidateQueryKey,
+    mutationOptions: {},
+  });
+};

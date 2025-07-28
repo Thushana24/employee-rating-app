0c7ee1d82183beeb7ea952c60fc25c6c6b71ba03
Thushana LS <dhushanals@example.com>
2025-07-28 12:20:27 +0530

    chore: update dependencies and add type definitions for auth
    - Added `argon2` and `@types/jsonwebtoken` to support secure password hashing and JWT typing
    - Added `migrate` script for Prisma schema synchronization and client generation
    - Bumped TypeScript version to 5.8.3
    - Created `RegisterInput` and `UserWithToken` types in `auth/types.ts` for improved type safety in registration logic

---
+++ src/app/api/auth/types.ts
@@ -0,0 +1,7 @@
+import RegisterUserSchema from "@/schemas/user.schema";
+import { User } from "@prisma/client";
+import z from "zod";
+
+export type UserWithToken = User & { token: string };
+
+export type RegisterInput = z.infer<typeof RegisterUserSchema>;
